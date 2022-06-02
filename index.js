9db2b32d772af72a156cb10d99f92bc39081ffa8
jainec <jayne-conceicao@hotmail.com>
2022-06-02 12:48:51 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log('Loading function');
+
+var elasticsearch = require('./elastic/elastic');
+
+elasticsearch.initIndex();
+
+exports.handler = function(event) {
+        var message = event.Records[0].Sns.Message;
+        console.log('Message received from SNS:', message);
+
+        elasticsearch.addDocument(JSON.parse(message));
+
+        console.log('Document added to elastic');
+
+        return 'Success';
+    };
